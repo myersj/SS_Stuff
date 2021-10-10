5e0b9db2d7764048f00910cf2e3b760065fbed4d
myersj <myersj@shawanoschools.com>
2021-10-10 14:34:22 +0000

    downgrade to Java8 for built in javafx

--- MyGraphicsLab1.java
+++ MyGraphicsLab1.java
@@ -5,11 +5,24 @@ import javafx.scene.shape.*;
 import javafx.scene.paint.Color;
 import javafx.scene.input.*;
 import javafx.beans.binding.DoubleBinding; import javafx.beans.property.DoubleProperty;
-public class MyLab
+public class MyGraphicsLab1
 {
     private static final int SCENE_WIDTH = 640;
     private static final int SCENE_HEIGHT = 480;
     private static final int CIRC_RADIUS = 35;
     private static final int CIRC_DIAMETER = 2 * CIRC_RADIUS;
-    public void start(Stage primaryStage) { }
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+
+    public void start(Stage primaryStage) { 
+        Circle circle = new Circle();
+        circle.setRadius(CIRC_RADIUS);
+        circle.setCenterX(0);
+        circle.setCenterY(0);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.rgb(new java.util.Random().nextInt(256),
+        new java.util.Random().nextInt(256),
+        new java.util.Random().nextInt(256)));
+    }
 }
